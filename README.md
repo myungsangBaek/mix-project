39b2c993d494b78822f7a0cbf70540a50968ac43
myungsangBaek <bms6168@naver.com>
2022-08-17 21:18:48 +0900

    feat: Add react query and recoil

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 ???
+
+```bash
+
+#Next.js
+ npx create-next-app@latest --typescript
+
+#Styled-components + TS + Babel + Reset
+npm install --save styled-components
+npm i --save-dev @types/styled-components
+
+npm install --save-dev babel-plugin-styled-components
+
+npm i styled-reset
+
+#react-query + recoil
+
+npm i recoil react-query
+```
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,28 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global-style";
+import { theme } from "../styles/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Mix</title>
       </Head>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
     </>
   );
 }
