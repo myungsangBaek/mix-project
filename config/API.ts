fe6cf56293cea4efe631d596fda015fb7c68afdf
myungsangBaek <bms6168@naver.com>
2022-08-18 21:55:24 +0900

    feat: Add api

---
+++ config/API.ts
@@ -0,0 +1,6 @@
+export type getBoardList = {
+  id?: string | null;
+  title: string;
+  rDate: Date;
+  like: number;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
 
 const Home: NextPage = () => {
-  return <div>Main</div>;
+  return <Container>Main</Container>;
 };
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 export default Home;
